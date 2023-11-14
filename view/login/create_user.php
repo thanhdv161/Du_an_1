6fba8a62ee713fd55533a1110d984b03175d8421
minhhv24 <minhhvph38395@fpt.edu.vn>
2023-11-14 23:32:00 +0700

    bo xung chuc nang dang ki

---
+++ view/login/create_user.php
@@ -0,0 +1,30 @@
+<?php
+include "../login/pdo.php";
+
+if (isset($_POST['submit1'])) {
+    $email = $_POST["email"];
+$username = $_POST["tenTK"];
+$password = $_POST["matKhau"];
+$location = $_POST["diaChi"];
+$roleId = $_POST["maVaiTro"];
+$sdt = $_POST["sdt"];
+$image = $_FILES["anh"]['name'];
+if (isset($_FILES['anh'])) {
+    $file = $_FILES['anh'];
+    $file_name = $file['name'];
+    move_uploaded_file($file['tmp_name'], './img/' . $file_name);
+}
+$query = "SELECT * FROM taikhoan WHERE email='$email'";
+$user = pdo_query($query);
+
+    if (!empty($user)) {
+        header("location:http://localhost/duan1/view/index.php?act=dangki&error");
+        die;
+    } else {
+        $query = "INSERT INTO taikhoan( tenTK, matKhau, email, diaChi, 
+        maVaiTro, anh, sdt) VALUES ('$username ','$password','$email','$location','$roleId','$image','$sdt')";
+        pdo_execute($query);
+        header("location:http://localhost/duan1/view/index.php?act=login");
+    }
+}
+
--- view/login/dangky.php
+++ view/login/dangky.php
@@ -3,16 +3,17 @@
 
 <div class="w-[500px] mx-auto container text-center">
         <h3 class="text-4xl font-bold my-5">Tạo Tài Khoản</h3>
-        <form action="./create_user.php" method="POST" enctype="multipart/form-data">
+        <form action="../view/login/create_user.php" method="POST" enctype="multipart/form-data">
 
             <input type="email" name="email" id="" placeholder="Email" class="inp33"><br>
             <input type="texk" name="tenTK" id="" placeholder="Tên Tài Khoản" class="inp33"><br>
             <input type="password" name="matKhau" id="" placeholder="Mật Khẩu" class="inp33"><br>
             <input type="file" name="anh" id="" class="inp33"><br>
+            <input type="text" value="2" name="maVaiTro" hidden>
             <input type="text" name="sdt" id="" placeholder="Số Điện Thoại" class="inp33"><br>
             <input type="text" name="diaChi" id="" placeholder="Địa Chỉ" class="inp33">
             <div class="text-left2">
-                <button class="px-5 text-black py-2  slide_right" >Đăng Kí</button>
+                <button class="px-5 text-black py-2  slide_right" type="submit" name="submit1" >Đăng Kí</button>
                 <a href="http://localhost/duan1/view/index.php?act=home" style="color: black;">Hủy</a>
             </div>
         </form>
