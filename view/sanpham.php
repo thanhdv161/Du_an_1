5fb46a07e12bb63283116bf80a5fd5d7a9f66656
huyvdph41392 <huyvdph41392@fpt.edu.vn>
2023-11-23 22:22:01 +0700

    feat: chức năng tìm kiếm sản phẩm

--- view/sanpham.php
+++ view/sanpham.php
@@ -4,92 +4,36 @@
                 <div class="f_p_inner">
                     <div class="row">
                         <div class="col-lg-3">
+                            <!--================top sản phẩm =================-->
                             <div class="f_product_left">
                                 <div class="f_product_inner">
                                     <div class="s_m_title">
                                         <h2>Top sản phẩm</h2>
                                     </div>
+                                    <?php foreach ($topsp as $hanghoa) : ?>
                                     <div class="media">
                                         <div class="d-flex">
-                                            <img src="../img/product/xiaomi_note12_128gb.png" alt="">
+                                        <a href="./index.php?act=chitietsanpham&id=<?php echo $hanghoa['maHH'] ?>">
+                                                    <img style="width: 80px;" src="../img/<?php $arr = explode(",", $hanghoa['anh']);
+                                                                                                    echo $arr[0];
+                                                                                                    ?>" alt=""></a>
                                         </div>
                                         <div class="media-body">
-                                            <h4>Xiaomi Note12 128gb</h4>
-                                            <h5>4.990.000đ</h5>
+                                            <h4><?php echo $hanghoa['tenHH'] ?></h4>
+                                            <h5><?php echo number_format($hanghoa['gia'])?>đ</h5>
                                         </div>
                                     </div>
-                                    <div class="media">
-                                        <div class="d-flex">
-                                            <img src="../img/product/samsung-galaxy-z-fold-5-256gb_1.png" alt="">
-                                        </div>
-                                        <div class="media-body">
-                                            <h4>Samsung Galaxy Z Fold5 12GB 256GB</h4>
-                                            <h5>30.790.000₫</h5>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <div class="d-flex">
-                                            <img src="../img/product/iphone-15-pro-max_3.png" alt="">
-                                        </div>
-                                        <div class="media-body">
-                                            <h4>iPhone 15 Pro Max 256GB</h4>
-                                            <h5>33.690.000đ</h5>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <div class="d-flex">
-                                            <img src="../img/product/realme-11-pro.png" alt="">
-                                        </div>
-                                        <div class="media-body">
-                                            <h4>realme 11 Pro (8GB - 256GB)</h4>
-                                            <h5>11.490.000đ</h5>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <div class="d-flex">
-                                            <img src="../img/product/xiaomi_note12_128gb.png" alt="">
-                                        </div>
-                                        <div class="media-body">
-                                            <h4>Xiaomi Note12 128gb</h4>
-                                            <h5>4.990.000đ</h5>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <div class="d-flex">
-                                            <img src="../img/product/samsung-galaxy-z-fold-5-256gb_1.png" alt="">
-                                        </div>
-                                        <div class="media-body">
-                                            <h4>Samsung Galaxy Z Fold5 12GB 256GB</h4>
-                                            <h5>30.790.000₫</h5>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <div class="d-flex">
-                                            <img src="../img/product/iphone-15-pro-max_3.png" alt="">
-                                        </div>
-                                        <div class="media-body">
-                                            <h4>iPhone 15 Pro Max 256GB</h4>
-                                            <h5>33.690.000đ</h5>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <div class="d-flex">
-                                            <img src="../img/product/realme-11-pro.png" alt="">
-                                        </div>
-                                        <div class="media-body">
-                                            <h4>realme 11 Pro (8GB - 256GB)</h4>
-                                            <h5>11.490.000đ</h5>
-                                        </div>
-                                    </div>
+                                    <?php endforeach; ?>
                                 </div>
                             </div>
+                             <!--================kết thúc top sản phẩm =================-->
                         </div>
                         <div class="col-lg-9">
                             <div class="s_m_title">
                                 <h2>Tất cả sản phẩm</h2>
                             </div>
                             <div class="sanpham-flex">
-                                <?php foreach ($product as $hanghoa) : ?>
+                                <?php foreach ($dssp as $hanghoa) : ?>
                                     <div class="sanphamdanhmuc">
                                         <div class="l_product_item">
                                             <div class="l_p_img">
@@ -109,7 +53,6 @@
                                     </div>
                                 <?php endforeach; ?>
                             </div>
-                            
                         </div>
                     </div>
                 </div>
